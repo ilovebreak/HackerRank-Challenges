a22a38c4208256b5c6869c62c910df214eca9f19
Yevgenia <gea74@inbox.ru>
2022-08-05 20:03:06 +0500

    Solved Athlete Sort

---
+++ Builds-In - Athlete Sort.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    nm = input().split()
+    n = int(nm[0])
+    m = int(nm[1])
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+    k = int(input())
+
+for elem in sorted(arr, key=lambda x: x[k]):
+    print(*elem)
+
+# Sample Input 0
+# 5 3
+# 10 2 5
+# 7 1 0
+# 9 9 9
+# 1 23 12
+# 6 5 9
+# 1
+# Sample Output 0
+# 7 1 0
+# 10 2 5
+# 6 5 9
+# 9 9 9
+# 1 23 12
