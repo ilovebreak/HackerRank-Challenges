19be14cfc35619d556c9769f7bec0063345b880e
Gordeyeva <Yevgenia.Gordeyeva@METRAN.local>
2022-08-10 10:16:06 +0500

    solved eye and identity

---
+++ Numpy - Eye and Identity.py
@@ -0,0 +1,15 @@
+import numpy
+numpy.set_printoptions(legacy = '1.13')
+
+n, m = 3, 3 #map(int, input().split())
+
+print(numpy.eye(n, m))
+
+# Sample Input
+
+# 3 3
+# Sample Output
+
+# [[ 1.  0.  0.]
+#  [ 0.  1.  0.]
+#  [ 0.  0.  1.]]
